b60ad724040dc6c72d269da1c5e4b2a566ebdc66
alegomonkey <74006840+alegomonkey@users.noreply.github.com>
2024-10-02 12:54:02 -0400

    More entries to scrape
    Added sections for Dublin core and Zotero. Need more entries for Zotero and Dublin core need to be checked for completion

--- BS_Webscrape.py
+++ BS_Webscrape.py
@@ -23,6 +23,7 @@ soup = BeautifulSoup(html, "lxml")
 
 all_articles = soup.find_all("div", class_="element-set")
 for article in all_articles:
+    ######## DUBLIN CORE ##########
     #title = article.find("div", class_="dublin-core-title")
     title = article.select_one('div#dublin-core-title .element-text')
     if(title != None):
@@ -30,6 +31,61 @@ for article in all_articles:
     description = article.select_one('div#dublin-core-description .element-text')
     if(description != None):
         print(description.text)
+    creator = article.select_one('div#dublin-core-creator .element-text')
+    if(creator != None):
+        print(creator.text)
+    date = article.select_one('div#dublin-core-date .element-text')
+    if(date != None):
+        print(date.text)
+    language = article.select_one('div#dublin-core-language .element-text')
+    if(language != None):
+        print(language.text)
+    ########### ENTRY TYPE (might not be filled?) #############
+    type1 = article.select_one('div#dublin-core-type .element-text')
+    if(type1 != None):
+        print(type1.text)
+    ###### Identifier usually is html for a link ######
+    identifier = article.select_one('div#dublin-core-identifier .element-text a')
+    if(identifier != None):
+        print(identifier)
+    coverage = article.select_one('div#dublin-core-coverage .element-text')
+    if(coverage != None):
+        print(coverage.text)
+
+    ########### IGNORING CONTRIBUTION FORM ############
+    ########### ZOTERO ###########
+    zotero_num_pages = article.select_one('div#zotero-num-pages .element-text')
+    if(zotero_num_pages != None):
+        print(zotero_num_pages.text)
+    zotero_place = article.select_one('div#zotero-place .element-text')
+    if(zotero_place != None):
+        print(zotero_place.text)
+    zotero_publisher = article.select_one('div#zotero-publisher .element-text')
+    if(zotero_publisher != None):
+        print(zotero_publisher.text)
+    zotero_issn = article.select_one('div#zotero-issn .element-text')
+    if(zotero_issn != None):
+        print(zotero_issn.text)
+    zotero_issue = article.select_one('div#zotero-issue .element-text')
+    if(zotero_issue != None):
+        print(zotero_issue.text)
+    zotero_publication_title = article.select_one('div#zotero-publication-title .element-text')
+    if(zotero_publication_title != None):
+        print(zotero_publication_title.text)
+    zotero_url = article.select_one('div#zotero-url .element-text')
+    if(zotero_url != None):
+        print(zotero_url.text)
+    zotero_volume = article.select_one('div#zotero-volume .element-text')
+    if(zotero_volume != None):
+        print(zotero_volume.text)
+
+    ############### TYPE AGAIN ##############
+    type2 = article.select_one('h2')
+    if(type2 != None):
+        print(type2.text)
+    
+    
+
     #print(authors_venue_year.get_text(strip=False))
 
 itemfiles = soup.select_one('div#itemfiles div.element-text a')
@@ -41,7 +97,7 @@ print(collection.text)
 
 # |||||||||||||||||| TAGS HERE do checks? |||||||||||||||||||||||||
 tags = soup.select('div#item-tags div.element-text a')
-for tag in tags:
-    print(tag.text)
+#for tag in tags:
+#    print(tag.text)
 
 print()
